1d05f945a7e1aae02941148b2b7c056ea5af6442
KeigeeDev <coral.khalilgibran@gmail.com>
2026-02-09 05:48:41 +0800

    feat: Add asynchronous weather data fetching and ignore .env file.

---
+++ script.js
@@ -0,0 +1,11 @@
+async function getWeatherData(city) {
+    const API_KEY = process.env.WEATHER_API_KEY;
+    const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
+
+    const response = await fetch(URL);
+    if (!response.ok) {
+        throw new Error('City not found');
+    }
+
+    return await response.json();
+}
