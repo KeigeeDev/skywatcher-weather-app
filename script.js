a0dfc8a9502615855ce0534a6c69dc76a25fdeef
KeigeeDev <coral.khalilgibran@gmail.com>
2026-02-09 21:28:36 +0800

    feat: enhance UI with a main container, default background image, and improved error message display

--- script.js
+++ script.js
@@ -67,5 +67,13 @@ function getWeatherCategory(condition) {
 }
 
 function handleError(city) {
-    errorMessageElement.innerText = `Uh oh! We couldn't find that "${city}".`;
+    errorMessageElement.innerHTML = `
+        <span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="error-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        </span>
+        <p>Uh oh! We couldn't find that "${city}".</p>
+        
+    `;
 }
